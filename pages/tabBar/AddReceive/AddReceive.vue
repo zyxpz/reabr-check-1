1ccaea733a67f0208d4663d5d826c21db8006793
MonroeC <782597551@qq.com>
2024-05-20 16:22:52 +0800

    初始化

---
+++ pages/tabBar/AddReceive/AddReceive.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<uni-section title="多行显示" type="line">
+			<uni-notice-bar text="uni-app 版正式发布，开发一次，同时发布iOS、Android、H5、微信小程序、支付宝小程序、百度小程序、头条小程序等7大平台。" />
+		</uni-section>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
